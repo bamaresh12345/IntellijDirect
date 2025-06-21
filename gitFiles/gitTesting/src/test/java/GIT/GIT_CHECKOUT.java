2c16687dd939af8b3253fce985062dabd3a4fa97
bamaresh12345 <bamaresh2012@gmail.com>
2025-06-20 23:17:22 -0400

    fristCommit

---
+++ gitFiles/gitTesting/src/test/java/GIT/GIT_CHECKOUT.java
@@ -0,0 +1,17 @@
+package GIT;
+
+public class GIT_CHECKOUT {
+
+    /*
+        1.  git checkout -b <branch_name> --> create a new branch and switch to it
+        2.  git checkout <branch_name> --> switch to an existing branch
+        3.  git checkout - --> switch to the previous branch
+        4.  git checkout . --> discard all changes in the working directory
+        5.  git checkout -- <file_name> --> discard changes in a specific file
+        6.  git checkout <commit_hash askdhjf;lkasd23> --> switch to a specific commit (detached HEAD state)
+        7.  git checkout -b <new_branch_name> <base_branch_name> --> create a new branch based on another branch
+        8.  git checkout --track <remote_branch_name> --> create a local branch that tracks a remote branch
+
+
+     */
+}
