697cae9a4a347b26037a22cbe61f1c3456b605b5
bamaresh12345 <bamaresh2012@gmail.com>
2025-06-20 23:09:19 -0400

    fristCommit

---
+++ gitFiles/IntellijDirectTest/src/test/java/Tests/testing.java
@@ -0,0 +1,12 @@
+package Tests;
+
+public class testing {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 11; i++) {
+            System.out.println( i+") Sri Raghavendraya Namaha");
+        }
+
+    }
+}
