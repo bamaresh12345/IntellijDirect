272658992e06a343ab90fe0619e0c21137a40e4f
bamaresh12345 <bamaresh2012@gmail.com>
2025-07-04 12:14:46 -0400

    Adding tow numbers Program

---
+++ gitFiles/gitTesting/src/test/java/Intellij/AddTwoNumbers.java
@@ -0,0 +1,24 @@
+package Intellij;
+
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        int num1 = 10;  // first number
+        int num2 = 20;  // second number
+
+
+        int num3 = 10;  // first number
+        int num4 = 20;  // second number
+
+        int num5 = 10;  // first number
+        int num6 = 20;  // second number
+
+        int sum1 = num1 + num2;
+        int sum2 = num3 + num4;
+        int sum3 = num5 + num6;
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum1);
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum2);
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum3);
+    }
+}
+
