2145f73e66963c73037163c8d4c5d8d0d808f177
bamaresh12345 <bamaresh2012@gmail.com>
2025-07-04 13:44:41 -0400

    Adding frsdfsdfaction numbers Program

--- gitFiles/gitTesting/src/test/java/Intellij/AddTwoNumbers.java
+++ gitFiles/gitTesting/src/test/java/Intellij/AddTwoNumbers.java
@@ -2,18 +2,17 @@ package Intellij;
 
 public class AddTwoNumbers {
     public static void main(String[] args) {
-        int rreebbssd = 10;  // first number
+        int myBranchNum1= 10;  // first number
         int num2 = 20;  // second number
 
-
-        int ytest = 38;  // 3 number
+        int num3 = 38;  // 3 number
         int num4 = 20;  // 4 number
 
         int num5 = 10;  // 5 number
         int num6 = 20;  // 6 number
 
         int sum2 = num3 + num4;
-        int sum1 = numBNoCommit + num2;
+        int sum1 = myBranchNum1 + num2;
         int sum3 = num5 + num6;
 
         System.out.println("asdfasdThe sum of " + numBNoCommit + " and " + num2 + " is: " + sum1);
--- gitFiles/gitTesting/src/test/java/Intellij/fraction.java
+++ gitFiles/gitTesting/src/test/java/Intellij/fraction.java
@@ -3,13 +3,13 @@ package Intellij;
 public class fraction {
 
     public static void main(String[] args) {
-        int number1 = 10;  // first number
+        int myBranchNum1 = 10;  // first number
         int num2 = 20;  // second number
 
 
-        int sum1 = number1 / num2;
+        int sum1 = myBranchNum1 / num2;
 
-        System.out.println("asdfasdThe sum of " + number1 + " and " + num2 + " is: " + sum1);
+        System.out.println("asdfasdThe sum of " + myBranchNum1 + " and " + num2 + " is: " + sum1);
 
 
     }
