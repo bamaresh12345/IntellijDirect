7247b8344d4c9323e62144f60b69fe771aad09b4
bamaresh12345 <bamaresh2012@gmail.com>
2025-07-04 12:19:19 -0400

    Adding multiply numbers Program

--- gitFiles/gitTesting/src/test/java/Intellij/AddTwoNumbers.java
+++ gitFiles/gitTesting/src/test/java/Intellij/AddTwoNumbers.java
@@ -2,23 +2,30 @@ package Intellij;
 
 public class AddTwoNumbers {
     public static void main(String[] args) {
-        int num1 = 10;  // first number
+        int numz = 10;  // first number
         int num2 = 20;  // second number
 
 
-        int num3 = 10;  // first number
-        int num4 = 20;  // second number
+        int num3 = 10;  // 3 number
+        int num4 = 20;  // 4 number
 
-        int num5 = 10;  // first number
-        int num6 = 20;  // second number
+        int num5 = 10;  // 5 number
+        int num6 = 20;  // 6 number
 
-        int sum1 = num1 + num2;
+        int sum1 = numz + num2;
         int sum2 = num3 + num4;
         int sum3 = num5 + num6;
 
-        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum1);
-        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum2);
-        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum3);
+        System.out.println("asdfasdThe sum of " + numz + " and " + num2 + " is: " + sum1);
+        System.out.println("asdfasdThe sum of " + numz + " and " + num2 + " is: " + sum2);
+        System.out.println("The sum of " + numz + " and " + num2 + " is: " + sum3);
+
+        System.out.println("asdfsaThe sum of " + numz + " and " + num2 + " is: " + sum1);
+        System.out.println("asdfasfThe sum of " + numz + " and " + num2 + " is: " + sum2);
+        System.out.println("asdfasdfThe sum of " + numz + " and " + num2 + " is: " + sum3);
+
+        //updated sme lins of code and added some more lines
+        // added new file and checking git status
     }
 }
 
