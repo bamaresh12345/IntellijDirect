8b460274dbc226e8c466e6b812f304c714d5fe82
bamaresh12345 <bamaresh2012@gmail.com>
2025-07-04 13:06:19 -0400

    Adding frsdfsdfaction numbers Program

--- gitFiles/gitTesting/src/test/java/Intellij/multiply2nubmer.java
+++ gitFiles/gitTesting/src/test/java/Intellij/multiply2nubmer.java
@@ -3,14 +3,14 @@ package Intellij;
 public class multiply2nubmer {
 
     public static void main(String[] args) {
-        int myBranchNum1 = 10;  // first number
+        int number1 = 10;  // first number
         int num2a = 20;  // second number
 
 
-        int sum1 = myBranchNum1 * num2a;
+        int sum1 = number1 * num2a;
 
-        System.out.println("asdfasdThe sum of " + myBranchNum1 + " and " + num2a + " is: " + sum1);
-        System.out.println("asdfasdThe sum of " + myBranchNum1 + " and " + num2a + " is: " + sum1);
+        System.out.println("asdfasdThe sum of " + number1 + " and " + num2a + " is: " + sum1);
+        System.out.println("asdfasdThe sum of " + number1 + " and " + num2a + " is: " + sum1);
 
 
     }
