e1301fff03d4b722021ee5078e7ecef2b7c4e5e0
bamaresh12345 <59543502+bamaresh12345@users.noreply.github.com>
2025-07-04 12:53:21 -0400

    Update AddTwoNumbers.java

--- gitFiles/gitTesting/src/test/java/Intellij/AddTwoNumbers.java
+++ gitFiles/gitTesting/src/test/java/Intellij/AddTwoNumbers.java
@@ -2,7 +2,7 @@ package Intellij;
 
 public class AddTwoNumbers {
     public static void main(String[] args) {
-        int numBNoCommit = 10;  // first number
+        int numBNoCommitMain = 10;  // first number
         int num2 = 20;  // second number
 
 
