b7102e918d4c6181932e2b99a27837bc4b3b792f
bamaresh12345 <bamaresh2012@gmail.com>
2025-07-04 12:51:54 -0400

    Adding fraction numbers Program

--- gitFiles/gitTesting/src/test/java/Intellij/AddTwoNumbers.java
+++ gitFiles/gitTesting/src/test/java/Intellij/AddTwoNumbers.java
@@ -2,9 +2,8 @@ package Intellij;
 
 public class AddTwoNumbers {
     public static void main(String[] args) {
-        int numBNoCommit = 10;  // first number
+        int numBNoCommit1234= 10;  // first number
         int num2 = 20;  // second number
-
 
         int num3 = 38;  // 3 number
         int num4 = 20;  // 4 number
@@ -13,19 +12,19 @@ public class AddTwoNumbers {
         int num6 = 20;  // 6 number
 
         int sum2 = num3 + num4;
-        int sum1 = numBNoCommit + num2;
+        int sum1 = numBNoCommit1234 + num2;
         int sum3 = num5 + num6;
 
-        System.out.println("asdfasdThe sum of " + numBNoCommit + " and " + num2 + " is: " + sum1);
-        System.out.println("The sum of " + numBNoCommit + " and " + num2 + " is: " + sum3);
+        System.out.println("asdfasdThe sum of " + numBNoCommit1234 + " and " + num2 + " is: " + sum1);
+        System.out.println("The sum of " + numBNoCommit1234 + " and " + num2 + " is: " + sum3);
 
-        System.out.println("asdfsaThe sum of " + numBNoCommit + " and " + num2 + " is: " + sum1);
-        System.out.println("asdfasfThe sum of " + numBNoCommit + " and " + num2 + " is: " + sum2);
-        System.out.println("asdfasdfThe sum of " + numBNoCommit + " and " + num2 + " is: " + sum3);
+        System.out.println("asdfsaThe sum of " + numBNoCommit1234 + " and " + num2 + " is: " + sum1);
+        System.out.println("asdfasfThe sum of " + numBNoCommit1234 + " and " + num2 + " is: " + sum2);
+        System.out.println("asdfasdfThe sum of " + numBNoCommit1234 + " and " + num2 + " is: " + sum3);
 
-        System.out.println("asdfsaThe sum of " + numBNoCommit + " and " + num2 + " is: " + sum1);
-        System.out.println("asdfasfThe sum of " + numBNoCommit + " and " + num2 + " is: " + sum2);
-        System.out.println("asdfasdfThe sum of " + numBNoCommit + " and " + num2 + " is: " + sum3);
+        System.out.println("asdfsaThe sum of " + numBNoCommit1234 + " and " + num2 + " is: " + sum1);
+        System.out.println("asdfasfThe sum of " + numBNoCommit1234 + " and " + num2 + " is: " + sum2);
+        System.out.println("asdfasdfThe sum of " + numBNoCommit1234 + " and " + num2 + " is: " + sum3);
 
         //updated sme lins of code and added some more lines
         // added new file and checking git status
