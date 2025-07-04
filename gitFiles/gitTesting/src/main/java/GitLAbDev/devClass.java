c37cac86c85261248b53502d046dda2b4fb44bc3
bamaresh12345 <bamaresh2012@gmail.com>
2025-07-04 19:24:34 -0400

    Initall commt

--- gitFiles/gitTesting/src/main/java/GitLAbDev/devClass.java
+++ gitFiles/gitTesting/src/main/java/GitLAbDev/devClass.java
@@ -3,6 +3,7 @@ package GitLAbDev;
 public class devClass {
 
     public static void main(String[] args) {
+
         System.out.println("Hello, Dev Class!");
     }
 }
---
+++ gitFiles/gitTesting/src/test/java/GitLab/testClass.java
@@ -0,0 +1,8 @@
+package GitLab;
+
+public class testClass {
+
+    public static void main(String[] args) {
+        System.out.println("test calss");
+    }
+}
