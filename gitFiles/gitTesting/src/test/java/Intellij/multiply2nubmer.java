b24f24ab9e8eb4d36c9be627e2c22810d9914724
bamaresh12345 <59543502+bamaresh12345@users.noreply.github.com>
2025-07-05 15:55:44 -0400

    Update multiply2nubmer.java

--- gitFiles/gitTesting/src/test/java/Intellij/multiply2nubmer.java
+++ gitFiles/gitTesting/src/test/java/Intellij/multiply2nubmer.java
@@ -6,11 +6,16 @@ public class multiply2nubmer {
         int myBranchNum1 = 10;  // first number
         int num2a = 20;  // second number
 
+        
+        // this is modified from master branch
 
-        int sum1 = myBranchNum1 * num2a;
+        int sum1 = myBranchNum1 * num2a;  //testing added frtom master branch
 
+        System.out.println("added from master branch sum of " + myBranchNum1 + " and " + num2a + " is: " + sum1);
         System.out.println("asdfasdThe sum of " + myBranchNum1 + " and " + num2a + " is: " + sum1);
-        System.out.println("asdfasdThe sum of " + myBranchNum1 + " and " + num2a + " is: " + sum1);
+
+        System.out.println("added from master branch sum of " + myBranchNum1 + " and " + num2a + " is: " + sum1);
+        System.out.println("added from master branch sum of " + myBranchNum1 + " and " + num2a + " is: " + sum1);
 
 
     }
