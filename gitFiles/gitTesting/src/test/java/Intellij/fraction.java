b0097df1f329c3a073c54d355ad3f48738dbeb9a
bamaresh12345 <59543502+bamaresh12345@users.noreply.github.com>
2025-07-05 16:23:00 -0400

    Update fraction.java

--- gitFiles/gitTesting/src/test/java/Intellij/fraction.java
+++ gitFiles/gitTesting/src/test/java/Intellij/fraction.java
@@ -6,12 +6,12 @@ public class fraction {
         int myBranchNum1 = 10;  // first number
         int num2 = 20;  // second number
 
-        // Editing files ,get merge comments , modified in master
+        // Editing files ,get merge comments , asdfasdfasdfasdf
         // this is modified get merge comments
 
         int sum1 = myBranchNum1 / num2;
 
-        System.out.println("get merge comments" + num1 + " and " + num2 + " is: " + sum1);  //// this is modified from master branch2
+        System.out.println("get masdfasdfasdnts" + num1 + " and " + num2 + " is: " + sum1);  //// this is modified from master branch2
         System.out.println("get merge comments" + num1 + " and " + num2 + " is: " + sum1);  //// this is modified from master branch2
          System.out.println("get merge comments" + num1 + " and " + num2 + " is: " + sum1); 
 
@@ -21,12 +21,7 @@ public class fraction {
         System.out.println("This modified from local Branch " + num1 + " and " + num2 + " is: " + sum1);// this is modifi
 
 
-  // added new lines in mastyer 
-          System.out.println("This modified from local Branch " + num1 + " and " + num2 + " is: " + sum1);// this is modifi
-          System.out.println("This modified from local Branch " + num1 + " and " + num2 + " is: " + sum1);// this is modifi
-          System.out.println("This modified from local Branch " + num1 + " and " + num2 + " is: " + sum1);// this is modifi
-          System.out.println("This modified from local Branch " + num1 + " and " + num2 + " is: " + sum1);// this is modifi
-          System.out.println("This modified from local Branch " + num1 + " and " + num2 + " is: " + sum1);// this is modifi
+  // added new lines in mastyer deleted som any lines
           System.out.println("This modified from local Branch " + num1 + " and " + num2 + " is: " + sum1);// this is modifi
         
 
