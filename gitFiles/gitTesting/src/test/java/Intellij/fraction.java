633b472b08a22249f5cf31d524c3a759a0fecf05
bamaresh12345 <59543502+bamaresh12345@users.noreply.github.com>
2025-07-05 16:10:19 -0400

    Update fraction.java

--- gitFiles/gitTesting/src/test/java/Intellij/fraction.java
+++ gitFiles/gitTesting/src/test/java/Intellij/fraction.java
@@ -6,19 +6,29 @@ public class fraction {
         int myBranchNum1 = 10;  // first number
         int num2 = 20;  // second number
 
-        // this is modified from loacal branch
-        // this is modified from master branch2
+        // Editing files ,get merge comments , modified in master
+        // this is modified get merge comments
 
         int sum1 = myBranchNum1 / num2;
 
-        System.out.println("This modified from local Branch " + num1 + " and " + num2 + " is: " + sum1);  //// this is modified from master branch2
-        System.out.println("This modified from local Branch " + num1 + " and " + num2 + " is: " + sum1);  //// this is modified from master branch2
-        System.out.println("asdfasdThe sum of " + myBranchNum1 + " and " + num2 + " is: " + sum1);
+        System.out.println("get merge comments" + num1 + " and " + num2 + " is: " + sum1);  //// this is modified from master branch2
+        System.out.println("get merge comments" + num1 + " and " + num2 + " is: " + sum1);  //// this is modified from master branch2
+         System.out.println("get merge comments" + num1 + " and " + num2 + " is: " + sum1); 
 
         System.out.println("This modified from local Branch " + num1 + " and " + num2 + " is: " + sum1); // this is modified from master branch2
         System.out.println("This modified from local Branch " + num1 + " and " + num2 + " is: " + sum1);// this is modified from master branch2
            System.out.println("This modified from local Branch " + num1 + " and " + num2 + " is: " + sum1); // this is modified from master branch2
         System.out.println("This modified from local Branch " + num1 + " and " + num2 + " is: " + sum1);// this is modifi
+
+
+  // added new lines in mastyer 
+          System.out.println("This modified from local Branch " + num1 + " and " + num2 + " is: " + sum1);// this is modifi
+          System.out.println("This modified from local Branch " + num1 + " and " + num2 + " is: " + sum1);// this is modifi
+          System.out.println("This modified from local Branch " + num1 + " and " + num2 + " is: " + sum1);// this is modifi
+          System.out.println("This modified from local Branch " + num1 + " and " + num2 + " is: " + sum1);// this is modifi
+          System.out.println("This modified from local Branch " + num1 + " and " + num2 + " is: " + sum1);// this is modifi
+          System.out.println("This modified from local Branch " + num1 + " and " + num2 + " is: " + sum1);// this is modifi
+        
 
     }
 }
