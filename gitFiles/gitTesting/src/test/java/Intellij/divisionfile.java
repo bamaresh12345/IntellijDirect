f81a5f9072a3ab8b3595642152efa4a640b8695b
bamaresh12345 <59543502+bamaresh12345@users.noreply.github.com>
2025-07-05 16:03:13 -0400

    Update divisionfile.java

--- gitFiles/gitTesting/src/test/java/Intellij/divisionfile.java
+++ gitFiles/gitTesting/src/test/java/Intellij/divisionfile.java
@@ -6,18 +6,21 @@ public class divisionfile {
         int myBranchNum1 = 10;  // first number
         int num2 = 20;  // second number
 
-        // this is modified from loacal branch
-
+        // this is modified from master branch
+        // this is modified from master branch2
         int sum1 = myBranchNum1 / num2; // its division
 
        //hello
         //hello
 
 
-        System.out.println("This modified from local  " + num1 + " and " + num2 + " is: " + sum1);
-        System.out.println("Switch branch sum of " + myBranchNum1 + " and " + num2 + " is: " + sum1);
+        System.out.println("This modified from local  " + num1 + " and " + num2 + " is: " + sum1); // this is modified from master branch2
+        System.out.println("Switch branch sum of " + myBranchNum1 + " and " + num2 + " is: " + sum1);// this is modified from master branch2
 
-        System.out.println("This modified from local Branch " + num1 + " and " + num2 + " is: " + sum1);
-        System.out.println("This modified from local Branch " + num1 + " and " + num2 + " is: " + sum1);
+        System.out.println("This modified from local Branch " + num1 + " and " + num2 + " is: " + sum1);// this is modified from master branch2
+        System.out.println("This modified from local Branch " + num1 + " and " + num2 + " is: " + sum1);// this is modified from master branch2
+
+         System.out.println("This modified from local Branch " + num1 + " and " + num2 + " is: " + sum1);// this is modified from master branch2
+        System.out.println("This modified from local Branch " + num1 + " and " + num2 + " is: " + sum1);// this is modified from master branch2
     }
 }
